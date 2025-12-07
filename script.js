65bb5026996addb6275f2261423a4440d7c33ff2
IkerRL <163485280+IkerRL@users.noreply.github.com>
2025-12-07 19:58:21 +0100

    Update script.js
    Updated the logo URL for 'Among Us'.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const gamesData = [
   { id: 'repo',        name: 'R.E.P.O',          logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD_F0HJlREbbCJy4wtWi0Yz0qWawUz1fpVJg&s' },
   { id: 'fortnite',    name: 'Fortnite',         logo: 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Fortnite_F_lettermark_logo.png' },
   { id: 'cs',     name: 'Cs',          logo: 'https://gaming-cdn.com/images/products/13664/orig/counter-strike-2-pc-juego-steam-cover.jpg?v=1695885435' },
-  { id: 'amongus',     name: 'Among Us',         logo: 'https://static.wikia.nocookie.net/among-us-wiki/images/8/84/Among_Us.png/revision/latest?cb=20240408020746' },
+  { id: 'amongus',     name: 'Among Us',         logo: 'https://logos-world.net/wp-content/uploads/2021/08/Among-Us-Logo.png' },
   { id: 'makeitmeme',  name: 'Make it Meme',     logo: 'https://colyseus.io/_astro/make-it-meme-logo.5c9b2cf4_iBIWc.webp' },
   { id: 'lethal',      name: 'Lethal Company',   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUuNUS1gkuSPWWWM2S-YoIsRifDASykH4HPQ&s' },
   { id: 'spiderman2',  name: 'SpiderMan 2',      logo: 'https://image.api.playstation.com/vulcan/ap/rnd/202306/1219/1c7b75d8ed9271516546560d219ad0b22ee0a263b4537bd8.png' },
